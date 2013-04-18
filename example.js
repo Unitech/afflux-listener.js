f5323993155573a608bfbdf7293cf635b8cb967c
Alexandre Strzelewicz <strzelewicz.alexandre@gmail.com>
2013-04-18 17:05:48 +0800

    fc

---
+++ example.js
@@ -0,0 +1,7 @@
+var AffluxListener = require('..');
+
+var a = new AffluxListener({port : 6380, host : '127.0.0.1'});
+
+a.on('message', function(data) {
+    console.log('New message = ', data);
+});
